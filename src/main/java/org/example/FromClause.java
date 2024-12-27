f8d77818ba0eda1f631f628f8eebe54c7c322ca1
chamikam <chamikamadhushan35@gmail.com>
2024-12-27 09:33:02 +0530

    from clause taken out

---
+++ src/main/java/org/example/FromClause.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+public class FromClause<T> implements PipelineStage<T> {
+    private final Iterable<T> collection;
+
+    public FromClause(Iterable<T> collection) {
+        this.collection = collection;
+    }
+
+    @Override
+    public Stream<T> apply(Stream<T> input) {
+        return StreamSupport.stream(collection.spliterator(), false);
+    }
+}
